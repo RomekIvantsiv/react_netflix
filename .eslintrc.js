3229de5efd9ebe3cad0d94db12b9041442b322e8
RomekIvantsiv <ivantsiv.romek@gmail.com>
2022-08-08 11:02:28 +0200

    ESLint configured

---
+++ .eslintrc.js
@@ -0,0 +1,75 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-shadow': 'off',
+    'arrow-body-style': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    // React
+    'react/prop-types': 0,
+    'react/self-closing-comp': 0,
+    'react/display-name': 0,
+
+    // JavaScript
+    semi: 0,
+    'no-proto': 0,
+    'no-unused-vars': 0,
+
+    // TypeScript
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/ban-types': 0,
+    '@typescript-eslint/semi': ['error'],
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': ['off'],
+    '@typescript-eslint/naming-convention': [
+      'off',
+      {
+        selector: 'default',
+        format: null,
+      },
+    ],
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
